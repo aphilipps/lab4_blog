0beb01d173685ddd4eadee9de189b972a565d01f
aphilipps <aphilipps9@gmail.com>
2016-07-31 16:57:03 -0400

    Finished workshop

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reducers from './reducers';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
 
-import App from './components/app';
+// import App from './components/app';
 
 import './style.scss';
 
-// entry point that just renders app
-// could be used for routing at some point
-ReactDOM.render(<App />, document.getElementById('main'));
+const store = createStore(reducers, {}, compose(
+  applyMiddleware(),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+// replace your ReactDOM render with the following
+// note this uses the Router stuff from last week
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>
+  , document.getElementById('main'));
